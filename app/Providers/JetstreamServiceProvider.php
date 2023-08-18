7c932eebaddc012b4f6c3aedea4f4d10833dc3be
Nicolas Kojima <nicolas.kojima@gmail.com>
2023-08-18 11:16:54 +0900

    added view permission and homepage to dashb

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -57,5 +57,9 @@ class JetstreamServiceProvider extends ServiceProvider
             'create',
             'update',
         ])->description('Editor users have the ability to read, create, and update.');
+
+        Jetstream::role('support', 'Support Specialist', [
+            'server:read',
+        ])->description('Support specialists can read server information.');
     }
 }
