829a7315bd7f963977a848b565f919c486def365
Nicolas Kojima <nicolas.kojima@gmail.com>
2023-08-18 11:29:32 +0900

    testing git

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,27 @@
+<style>
+    .post-grid {
+        margin-top: 5vh;
+        display: grid;
+        grid-template-columns: 60% 40%; 
+        grid-template-rows: min-content; 
+        position: relative;
+      }
+
+</style>
+
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+            <div class="post-grid">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Dashboard') }}
+                </h2>
+                <a href="homepage" class="font-semibold text-xl text-gray-800 leading-tight">
+                    Homepage
+                </a>    
+            </div>  
+        
     </x-slot>
+    
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
