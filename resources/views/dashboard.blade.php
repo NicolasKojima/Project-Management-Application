d0cb432eab32cdde11cbfef9a24055b728555ddb
Nicolas Kojima <nicolas.kojima@gmail.com>
2023-08-18 09:54:03 +0900

    morning of 18/08

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,23 @@
+<style>
+    .post-grid {
+        margin-top: 5vh;
+        display: grid;
+        grid-template-columns: 60% 40%; 
+        grid-template-rows: min-content; 
+        position: relative;
+      }
+</style>
+
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="post-grid">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+            <h2>
+                <a class="font-semibold text-xl text-gray-800 leading-tight" href="homepage" class="btn btn-primary my-2" > Homepage</a>
+            </h2>           
+        </div>  
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
-        </div>
-    </div>
 </x-app-layout>
