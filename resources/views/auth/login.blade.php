e99ca21c7b872fba3a9f59c27e405640ef7a581c
Nicolas Kojima <nicolas.kojima@gmail.com>
2024-01-18 10:20:51 +0900

    timetabole isnt working

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,14 @@
+<?php
+header("X-Frame-Options: SAMEORIGIN");
+header('X-Content-Type-Options: nosniff');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Page Title</title>
-
-    
 
     <style>
         body {
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,8 @@
+<?php
+header("X-Frame-Options: SAMEORIGIN");
+header('X-Content-Type-Options: nosniff');
+
+?>
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
