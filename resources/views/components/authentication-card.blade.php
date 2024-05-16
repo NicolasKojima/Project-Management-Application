a8feb6f96b1c0dabca5718022631c9c948c93322
NicolasKojima <nicolas.kojima@gmail.com>
2024-05-16 11:46:20 +0900

    5/16

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="background-color:#160da2">
     <div>
         {{ $logo }}
     </div>
